ca00c771d76db4dbb7b2fbfc59484c6b9aa61d42
Lokee86 <otstructures@gmail.com>
2026-05-16 12:27:56 -0700

    server receiving and decoding packet

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/websocket"
 )
+
+type InputPacket struct {
+	Type  string `json:"type"`
+	Input struct {
+		Forward bool `json:"forward"`
+		Back    bool `json:"back"`
+		Right   bool `json:"right"`
+		Left    bool `json:"left"`
+		Shoot   bool `json:"shoot"`
+	} `json:"input"`
+}
 
 func main() {
 	mux := http.NewServeMux()
@@ -48,13 +60,20 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 			break
 		}
 
-		log.Printf("Received: %s\n", msg)
+		var packet InputPacket
+
+		err = json.Unmarshal(msg, &packet)
+		if err != nil {
+			log.Println("bad packet:", err)
+			continue
+		}
+
+		log.Printf("Decoded packet: %+v\n", packet)
 
 		err = conn.WriteMessage(messageType, msg)
 		if err != nil {
 			log.Println(err)
 			break
 		}
-		log.Printf("Received: %s\n", msg)
 	}
 }
