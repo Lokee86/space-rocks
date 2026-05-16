7baa6eef2113f40a45c4d885526837dc4b298a6a
Lokee86 <otstructures@gmail.com>
2026-05-16 09:26:47 -0700

    echo websocket handler created

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -4,12 +4,15 @@ import (
 	"log"
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
 func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /health", healthHandler)
+	mux.HandleFunc("GET /ws", wsHandler)
 
 	fmt.Println("Server starting on :8080")
 
@@ -19,4 +22,38 @@ func main() {
 
 func healthHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("OK"))
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	var upgrader = websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	defer conn.Close()
+
+	log.Println("client connected")
+
+	for {
+		messageType, msg, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			break
+		}
+		
+		log.Printf("Received: %s\n")
+
+		err = conn.WriteMessage(messageType, msg)
+		if err != nil {
+			log.Println(err)
+			break
+		}
+	}
 }
