7a3acc4a551ea1e13b10b2c5ca4fd4e4b9793a8d
Lokee86 <otstructures@gmail.com>
2026-05-26 01:01:44 -0700

    Replace deleted game shell with session boot seams

---
+++ client/scripts/boot/session_boot_controller.gd
@@ -0,0 +1,54 @@
+extends Node
+
+const ClientConnectionService := preload("res://scripts/networking/client_connection_service.gd")
+const ShellBootFlow := preload("res://scripts/shell/shell_boot_flow.gd")
+const ClientSessionContext := preload("res://scripts/shell/client_session_context.gd")
+const Constants := preload("res://scripts/constants/constants.gd")
+
+var connection_service
+var shell_boot_flow
+var session_context
+var websocket_url := Constants.MULTIPLAYER_WS_URL
+var logger: Callable
+
+
+func configure(websocket_url_value: String, logger_callable: Callable) -> void:
+	websocket_url = websocket_url_value
+	logger = logger_callable
+
+
+func _ready() -> void:
+	session_context = ClientSessionContext.new()
+	connection_service = ClientConnectionService.new()
+	add_child(connection_service)
+	shell_boot_flow = ShellBootFlow.new(connection_service, websocket_url, logger)
+
+
+func request_single_player() -> void:
+	session_context.request_single_player()
+	shell_boot_flow.request_single_player()
+	shell_boot_flow.connect_to_game_server("single player")
+
+
+func request_create_room() -> void:
+	session_context.request_multiplayer()
+	shell_boot_flow.request_create_room()
+	shell_boot_flow.connect_to_game_server("multiplayer create")
+
+
+func request_join_room(room_code: String) -> void:
+	session_context.request_multiplayer()
+	shell_boot_flow.request_join_room(room_code)
+	shell_boot_flow.connect_to_game_server("multiplayer join: %s" % room_code)
+
+
+func get_connection_service():
+	return connection_service
+
+
+func get_shell_boot_flow():
+	return shell_boot_flow
+
+
+func get_session_context():
+	return session_context
---
+++ client/scripts/shell/app_entry.gd
@@ -0,0 +1,59 @@
+extends Node2D
+
+const SessionBootController := preload("res://scripts/boot/session_boot_controller.gd")
+const MainMenuSessionController := preload("res://scripts/main_menu/main_menu_session_controller.gd")
+const Constants := preload("res://scripts/constants/constants.gd")
+const ClientLogger := preload("res://scripts/logging/logger.gd")
+
+@onready var main_menu: Control = $CanvasLayer/MainMenu
+
+var session_boot_controller
+var main_menu_session_controller
+
+
+func _ready() -> void:
+	_log_v2_status("V2 app entry booted")
+	session_boot_controller = SessionBootController.new()
+	session_boot_controller.configure(Constants.MULTIPLAYER_WS_URL, Callable(self, "_log_v2_status"))
+	add_child(session_boot_controller)
+	main_menu_session_controller = MainMenuSessionController.new()
+	main_menu_session_controller.configure(
+		main_menu,
+		session_boot_controller,
+		Callable(self, "_log_v2_status")
+	)
+	_connect_main_menu_signals()
+
+
+func _connect_main_menu_signals() -> void:
+	if main_menu == null:
+		push_error("Missing main menu")
+		return
+
+	_connect_main_menu_signal("single_player_pressed", _on_single_player_pressed)
+	_connect_main_menu_signal("multiplayer_create_requested", _on_multiplayer_create_requested)
+	_connect_main_menu_signal("multiplayer_join_requested", _on_multiplayer_join_requested)
+
+
+func _connect_main_menu_signal(signal_name: StringName, handler: Callable) -> void:
+	if main_menu.has_signal(signal_name):
+		main_menu.connect(signal_name, handler)
+
+
+func _log_v2_status(message: String) -> void:
+	ClientLogger.shell_info(message)
+
+
+func _on_single_player_pressed() -> void:
+	_log_v2_status("V2 app entry single player requested")
+	main_menu_session_controller.request_single_player()
+
+
+func _on_multiplayer_create_requested() -> void:
+	_log_v2_status("V2 app entry multiplayer create requested")
+	main_menu_session_controller.request_create_room()
+
+
+func _on_multiplayer_join_requested(room_code: String) -> void:
+	_log_v2_status("V2 app entry multiplayer join requested: %s" % room_code)
+	main_menu_session_controller.request_join_room(room_code)
