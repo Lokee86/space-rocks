f0b236441541912d85d573acfbdbc04cee1c5422
unknown <laughingskullhq@gmail.com>
2026-06-02 14:40:34 -0700

    refactor: split client inbound packet dispatch

--- client/scripts/networking/client_connection_service.gd
+++ client/scripts/networking/client_connection_service.gd
@@ -1,8 +1,7 @@
 extends Node
 
 const NetworkClientScript := preload("res://scripts/networking/network_client.gd")
-const ServerPacketRouter := preload("res://scripts/networking/packets/server_packet_router.gd")
-const ClientLogger := preload("res://scripts/logging/logger.gd")
+const ServerPacketDispatcher := preload("res://scripts/networking/inbound/server_packet_dispatcher.gd")
 const Packets := preload("res://scripts/networking/packets/packets.gd")
 const Constants := preload("res://scripts/constants/constants.gd")
 
@@ -18,13 +17,17 @@ signal telemetry_pong_received(packet: Dictionary)
 signal unknown_packet_received(packet: Dictionary)
 
 var network_client: NetworkClient
+var server_packet_dispatcher: ServerPacketDispatcher
 var has_started_connection := false
 
 
 func _ready() -> void:
 	process_priority = Constants.NETWORK_POLL_PROCESS_PRIORITY
 	network_client = NetworkClientScript.new()
+	server_packet_dispatcher = ServerPacketDispatcher.new()
 	add_child(network_client)
+	add_child(server_packet_dispatcher)
+	_connect_server_packet_dispatcher_signals()
 	_connect_network_client_signals()
 
 
@@ -124,9 +127,24 @@ func _connect_network_client_signals() -> void:
 	_connect_network_signal("packet_received", Callable(self, "_on_packet_received"))
 
 
+func _connect_server_packet_dispatcher_signals() -> void:
+	_connect_dispatcher_signal("room_snapshot_received", Callable(self, "_on_room_snapshot_received"))
+	_connect_dispatcher_signal("room_state_changed", Callable(self, "_on_room_state_changed"))
+	_connect_dispatcher_signal("room_error_received", Callable(self, "_on_room_error_received"))
+	_connect_dispatcher_signal("gameplay_state_received", Callable(self, "_on_gameplay_state_received"))
+	_connect_dispatcher_signal("player_pause_state_received", Callable(self, "_on_player_pause_state_received"))
+	_connect_dispatcher_signal("telemetry_pong_received", Callable(self, "_on_telemetry_pong_received"))
+	_connect_dispatcher_signal("unknown_packet_received", Callable(self, "_on_unknown_packet_received"))
+
+
 func _connect_network_signal(signal_name: StringName, handler: Callable) -> void:
 	if network_client.has_signal(signal_name):
 		network_client.connect(signal_name, handler)
+
+
+func _connect_dispatcher_signal(signal_name: StringName, handler: Callable) -> void:
+	if server_packet_dispatcher.has_signal(signal_name):
+		server_packet_dispatcher.connect(signal_name, handler)
 
 
 func _on_connected() -> void:
@@ -142,17 +160,33 @@ func _on_packet_parse_failed(text: String) -> void:
 
 
 func _on_packet_received(packet: Dictionary) -> void:
-	if ServerPacketRouter.is_room_snapshot(packet):
-		room_snapshot_received.emit(packet)
-	elif ServerPacketRouter.is_room_state_changed(packet):
-		room_state_changed.emit(packet)
-	elif ServerPacketRouter.is_room_error(packet):
-		room_error_received.emit(packet)
-	elif ServerPacketRouter.is_gameplay_state(packet):
-		gameplay_state_received.emit(packet)
-	elif ServerPacketRouter.is_player_pause_state(packet):
-		player_pause_state_received.emit(packet)
-	elif ServerPacketRouter.is_telemetry_pong(packet):
-		telemetry_pong_received.emit(packet)
-	else:
-		unknown_packet_received.emit(packet)
+	if server_packet_dispatcher != null:
+		server_packet_dispatcher.dispatch(packet)
+
+
+func _on_room_snapshot_received(packet: Dictionary) -> void:
+	room_snapshot_received.emit(packet)
+
+
+func _on_room_state_changed(packet: Dictionary) -> void:
+	room_state_changed.emit(packet)
+
+
+func _on_room_error_received(packet: Dictionary) -> void:
+	room_error_received.emit(packet)
+
+
+func _on_gameplay_state_received(packet: Dictionary) -> void:
+	gameplay_state_received.emit(packet)
+
+
+func _on_player_pause_state_received(packet: Dictionary) -> void:
+	player_pause_state_received.emit(packet)
+
+
+func _on_telemetry_pong_received(packet: Dictionary) -> void:
+	telemetry_pong_received.emit(packet)
+
+
+func _on_unknown_packet_received(packet: Dictionary) -> void:
+	unknown_packet_received.emit(packet)
