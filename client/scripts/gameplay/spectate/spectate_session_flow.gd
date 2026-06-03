0370b76cb1fc3fe140804d9f41059bc1a708bd4e
unknown <laughingskullhq@gmail.com>
2026-06-02 22:26:51 -0700

    tweaked graphics

--- client/scripts/gameplay/spectate/spectate_session_flow.gd
+++ client/scripts/gameplay/spectate/spectate_session_flow.gd
@@ -8,14 +8,15 @@ var gameplay_shell_flow
 var spectate_menu_state
 
 
-func configure(gameplay_menu_flow_ref, gameplay_shell_flow_ref) -> void:
+func configure(gameplay_menu_flow_ref, gameplay_shell_flow_ref, spectate_menu_state_ref = null) -> void:
 	gameplay_menu_flow = gameplay_menu_flow_ref
 	gameplay_shell_flow = gameplay_shell_flow_ref
-	spectate_menu_state = SpectateMenuState.new()
+	if spectate_menu_state_ref != null:
+		spectate_menu_state = spectate_menu_state_ref
+	else:
+		spectate_menu_state = SpectateMenuState.new()
 	if gameplay_menu_flow != null && gameplay_menu_flow.has_method("configure_spectate_menu_state"):
 		gameplay_menu_flow.configure_spectate_menu_state(spectate_menu_state)
-	if gameplay_shell_flow != null && gameplay_shell_flow.has_method("configure_spectate_menu_state"):
-		gameplay_shell_flow.configure_spectate_menu_state(spectate_menu_state)
 
 
 func apply_gameplay_state(state: Dictionary) -> void:
