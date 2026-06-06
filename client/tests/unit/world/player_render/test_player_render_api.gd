33d78ea21e99270e36491a54a5d5a1da1a891653
unknown <laughingskullhq@gmail.com>
2026-06-05 19:16:30 -0700

    fixe hitbox desync at high speeds

--- client/tests/unit/world/player_render/test_player_render_api.gd
+++ client/tests/unit/world/player_render/test_player_render_api.gd
@@ -45,6 +45,7 @@ func test_apply_state_uses_self_position_when_no_view_target_is_set() -> void:
 	api.apply_state(self_id, server_players)
 
 	assert_eq(api.server_position(), Vector2(100.0, 200.0))
+	api.interpolate(1.0, self_id)
 	assert_eq(view_anchor.global_position, api.visual_position())
 
 
@@ -84,4 +85,65 @@ func test_apply_state_falls_back_to_self_position_when_view_target_missing() -> 
 	api.apply_state(self_id, server_players)
 
 	assert_eq(api.server_position(), Vector2(100.0, 200.0))
+
+
+func test_apply_state_sets_view_anchor_target_without_snapping_after_initial_position() -> void:
+	var self_id := "player-1"
+	var first_state := {
+		self_id: {
+			Packets.FIELD_X: 100.0,
+			Packets.FIELD_Y: 200.0,
+			Packets.FIELD_ROTATION: 0.0,
+		}
+	}
+	var second_state := {
+		self_id: {
+			Packets.FIELD_X: 300.0,
+			Packets.FIELD_Y: 200.0,
+			Packets.FIELD_ROTATION: 0.0,
+		}
+	}
+
+	api.apply_state(self_id, first_state)
+	api.interpolate(1.0, self_id)
+
+	assert_eq(view_anchor.global_position, Vector2(100.0, 200.0))
+
+	api.apply_state(self_id, second_state)
+
+	assert_ne(view_anchor.global_position, Vector2(300.0, 200.0))
+	assert_eq(api.server_position(), Vector2(300.0, 200.0))
+
+	api.interpolate(1.0, self_id)
+
+	assert_eq(view_anchor.global_position, Vector2(300.0, 200.0))
+
+
+func test_view_anchor_rotation_smooths_through_interpolate() -> void:
+	var self_id := "player-1"
+	var first_state := {
+		self_id: {
+			Packets.FIELD_X: 100.0,
+			Packets.FIELD_Y: 200.0,
+			Packets.FIELD_ROTATION: 0.0,
+		}
+	}
+	var second_state := {
+		self_id: {
+			Packets.FIELD_X: 100.0,
+			Packets.FIELD_Y: 200.0,
+			Packets.FIELD_ROTATION: 1.0,
+		}
+	}
+
+	api.apply_state(self_id, first_state)
+	api.interpolate(1.0, self_id)
+
+	api.apply_state(self_id, second_state)
+
+	assert_ne(view_anchor.rotation, 1.0)
+
+	api.interpolate(1.0, self_id)
+
+	assert_eq(view_anchor.rotation, 1.0)
 
