6a748b524ef1b180b2813c58f4cfcfbbaaf6c0c8
Brian Fernstrom <otstructures@gmail.com>
2026-06-12 10:42:26 -0700

    Wire single-player pregame launch

--- client/tests/unit/ui/menu_flow/test_app_entry_menu_flow.gd
+++ client/tests/unit/ui/menu_flow/test_app_entry_menu_flow.gd
@@ -18,6 +18,29 @@ func test_single_player_button_routes_to_single_player_pregame() -> void:
 	assert_eq((pregame_menu.get_node_or_null("%ModeLabel") as Label).text, "SINGLE PLAYER")
 
 
+func test_single_player_pregame_play_endless_starts_game() -> void:
+	var game := await _create_game()
+	var main_menu := game.get_node("%MainMenu") as Control
+	var single_player_button := main_menu.get_node("%SinglePlayerButton") as BaseButton
+
+	single_player_button.emit_signal("pressed")
+	await get_tree().process_frame
+	await get_tree().process_frame
+
+	var canvas_layer := game.get_node("CanvasLayer") as CanvasLayer
+	var pregame_menu := _find_pregame_menu(canvas_layer)
+	assert_not_null(pregame_menu)
+	assert_eq((pregame_menu.get_node_or_null("%ModeLabel") as Label).text, "SINGLE PLAYER")
+
+	(pregame_menu.get_node("%EndlessCreateButton") as BaseButton).emit_signal("pressed")
+	await get_tree().process_frame
+	await get_tree().process_frame
+	await get_tree().process_frame
+
+	assert_null(_find_pregame_menu(canvas_layer))
+	assert_false(main_menu.visible)
+
+
 func test_multiplayer_button_routes_to_multiplayer_pregame() -> void:
 	var game := await _create_game()
 	var main_menu := game.get_node("%MainMenu") as Control
