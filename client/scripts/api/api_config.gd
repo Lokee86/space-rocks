d6796cc8f3e6fe7743629a93223015a302116a7a
Brian Fernstrom <otstructures@gmail.com>
2026-07-29 17:50:47 -0700

    build(client): finalize multiplayer alpha targets

--- client/scripts/api/api_config.gd
+++ client/scripts/api/api_config.gd
@@ -1,8 +1,8 @@
 class_name ApiConfig
 extends RefCounted
 
-const RAILS_API_BASE_URL := "https://space-rocks.laughingskull.ca"
-const DATA_HANDLER_API_BASE_URL := "https://space-rocks.laughingskull.ca/game"
+const RAILS_API_BASE_URL := "https://api.laughingskull.ca"
+const DATA_HANDLER_API_BASE_URL := "https://game.laughingskull.ca"
 const LOCAL_PACKAGED_ALPHA_FEATURE := "local_packaged_alpha"
 const LOCAL_SERVER_PORT_ENV := "SPACE_ROCKS_LOCAL_SERVER_PORT"
 const DEFAULT_LOCAL_SERVER_PORT := 8080
